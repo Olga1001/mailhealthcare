6fd91521f90f373be72da8f30936c86a528f4fb2
Olga <zhuravel.olga95@gmail.com>
2020-12-22 17:08:18 +0200

    Drug Directory page

---
+++ dist/js/main.js
@@ -0,0 +1,32 @@
+"use strict";
+
+$(".slider-medications").slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  dots: false,
+  arrows: false,
+  responsive: [{
+    breakpoint: 992,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      dots: true
+    }
+  }, {
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      dots: true
+    }
+  }, {
+    breakpoint: 576,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      variableWidth: true
+    }
+  }]
+});
